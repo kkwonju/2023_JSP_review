18264d14cfcd2d086aca4ececeb5dde82d3f2857
kkwonju <kkwo4140@gmail.com>
2023-04-07 07:24:21 +0900

    서블릿 생성

---
+++ src/main/java/com/kkwo/JAM/HomeMainServlet.java
@@ -0,0 +1,16 @@
+package com.kkwo.JAM;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Home/main")
+public class HomeMainServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Hello world!").append(request.getContextPath());
+	}
+}
